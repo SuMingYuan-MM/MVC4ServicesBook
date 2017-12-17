5a9844187e810c0d7fe63273ebf08e84f0c978ab
Myat Linn Maung <sumingyuan.mm@gmail.com>
2017-12-17 11:16:16 +0800

    Baseline Codes

---
+++ MVC4ServicesBook.Web.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MVC4ServicesBook.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
